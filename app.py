97dd43bd840c2855c3707edad9efda78ed5a96aa
Christopher Judd <christopher.judd.13@gmail.com>
2025-05-06 21:40:17 -0400

    added a datatable to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, dash_table
 import plotly.express as px
 import pandas as pd
 
@@ -10,7 +10,8 @@ server = app.server
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    dcc.Graph(id='graph-content'),
+    dash_table.DataTable(df.head(5).to_dict('records'), [{"name": i, "id": i} for i in df.columns])
 ])
 
 @callback(
